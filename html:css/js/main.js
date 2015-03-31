73deb8b080599d0bf83cc2e3ae8018cc60d18ea2
Jan Dočekal <honza.doc@gmail.com>
2015-03-31 15:56:29 +0200

    javascript
    shrink function yay! ;D

---
+++ html:css/js/main.js
@@ -0,0 +1,28 @@
+
+/* headnav scroll size */
+$(function(){
+    $('#headnav').data('size','big');
+});
+
+$(window).scroll(function(){
+    var $nav = $('#headnav');
+    if ($('body').scrollTop() > 0) {
+        if ($nav.data('size') == 'big') {
+            $nav.data('size','small').stop().animate({
+                height:'45px'
+            }, 600);
+            $("#headnav img").animate({
+                height:'35px'
+            }, 600);
+        }
+    } else {
+        if ($nav.data('size') == 'small') {
+            $nav.data('size','big').stop().animate({
+                height:'90px'
+            }, 600);
+            $("#headnav img").animate({
+                height:'80px'
+            }, 600);
+        }
+    }
+});
