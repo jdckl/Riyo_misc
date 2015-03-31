a9721fc3d9395096bcc0ea031ad41d7183436463
Jan Dočekal <honza.doc@gmail.com>
2015-03-31 16:08:49 +0200

    fx

--- html:css/js/main.js
+++ html:css/js/main.js
@@ -9,7 +9,8 @@ $(window).scroll(function(){
     if ($('body').scrollTop() > 0) {
         if ($nav.data('size') == 'big') {
             $nav.data('size','small').stop().animate({
-                height:'45px'
+                height:'45px',
+                opacity: 0.8
             }, 600);
             $("#headnav img").animate({
                 height:'35px'
@@ -18,7 +19,8 @@ $(window).scroll(function(){
     } else {
         if ($nav.data('size') == 'small') {
             $nav.data('size','big').stop().animate({
-                height:'90px'
+                height:'90px',
+                opacity: 1
             }, 600);
             $("#headnav img").animate({
                 height:'80px'
